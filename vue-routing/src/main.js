2dddd55040c91090677ced18c3b32bac5c6cc71d
Fabio Bento Luiz <bento_85@hotmail.com>
2021-04-20 20:58:28 +0200

    multiple routes with named router views

--- vue-routing/src/main.js
+++ vue-routing/src/main.js
@@ -6,16 +6,18 @@ import TeamsList from './components/teams/TeamsList.vue'
 import UsersList from './components/users/UsersList.vue'
 import TeamMembers from './components/teams/TeamMembers.vue';
 import NotFound from './components/nav/NotFound.vue';
+import TeamsFooter from './components/teams/TeamsFooter.vue';
+import UsersFooter from './components/users/UsersFooter.vue';
 
 const router = createRouter({
     history: createWebHistory(),
     routes: [
         {path: '/', redirect: '/teams'},
         // {path: '/teams', component: TeamsList, alias:'/' },
-        {path: '/teams', component: TeamsList, name: 'teams', children: [
+        {path: '/teams', components: {default: TeamsList, footer: TeamsFooter}, name: 'teams', children: [
             {path: ':teamId', component: TeamMembers, name: 'team-members', props: true },
         ] },
-        {path: '/users', component: UsersList },
+        {path: '/users', components: {default: UsersList, footer: UsersFooter} },
         // {path: '/:notFound(.*)', redirect: '/teams'}
         {path: '/:notFound(.*)', component: NotFound}
     ]
