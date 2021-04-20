09420ebf028ccf957e223421817d15f51041e29b
Fabio Bento Luiz <bento_85@hotmail.com>
2021-04-20 21:34:27 +0200

    route guards

--- vue-routing/src/main.js
+++ vue-routing/src/main.js
@@ -2,34 +2,57 @@ import { createApp } from 'vue';
 import { createRouter, createWebHistory } from 'vue-router';
 
 import App from './App.vue';
-import TeamsList from './components/teams/TeamsList.vue'
-import UsersList from './components/users/UsersList.vue'
+import TeamsList from './components/teams/TeamsList.vue';
+import UsersList from './components/users/UsersList.vue';
 import TeamMembers from './components/teams/TeamMembers.vue';
 import NotFound from './components/nav/NotFound.vue';
 import TeamsFooter from './components/teams/TeamsFooter.vue';
 import UsersFooter from './components/users/UsersFooter.vue';
 
 const router = createRouter({
-    history: createWebHistory(),
-    routes: [
-        {path: '/', redirect: '/teams'},
-        // {path: '/teams', component: TeamsList, alias:'/' },
-        {path: '/teams', components: {default: TeamsList, footer: TeamsFooter}, name: 'teams', children: [
-            {path: ':teamId', component: TeamMembers, name: 'team-members', props: true },
-        ] },
-        {path: '/users', components: {default: UsersList, footer: UsersFooter} },
-        // {path: '/:notFound(.*)', redirect: '/teams'}
-        {path: '/:notFound(.*)', component: NotFound}
-    ],
-    scrollBehavior(to, from, savedPosition) {
-        console.log(to, from, savedPosition);
-        if(savedPosition) {
-            return savedPosition;
+  history: createWebHistory(),
+  routes: [
+    { path: '/', redirect: '/teams' },
+    // {path: '/teams', component: TeamsList, alias:'/' },
+    {
+      path: '/teams',
+      components: { default: TeamsList, footer: TeamsFooter },
+      name: 'teams',
+      children: [
+        {
+          path: ':teamId',
+          component: TeamMembers,
+          name: 'team-members',
+          props: true
         }
-
-        // jump to the top
-        return {left: 0, top: 0}
+      ]
+    },
+    {
+      path: '/users',
+      components: { default: UsersList, footer: UsersFooter },
+      beforeEnter(to, from, next) {
+        console.log(to, from);
+        next();
+      }
+    },
+    // {path: '/:notFound(.*)', redirect: '/teams'}
+    { path: '/:notFound(.*)', component: NotFound }
+  ],
+  scrollBehavior(to, from, savedPosition) {
+    console.log(to, from, savedPosition);
+    if (savedPosition) {
+      return savedPosition;
     }
+
+    // jump to the top
+    return { left: 0, top: 0 };
+  }
+});
+
+router.beforeEach(function(to, from, next) {
+  // this is global
+  console.log(to, from);
+  next();
 });
 
 const app = createApp(App);
