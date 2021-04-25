c78509f2afa6542ffe22be96fe3d0a91873be920
Fabio Bento Luiz <bento_85@hotmail.com>
2021-04-25 19:16:33 +0200

    modal animation

--- vue-animation/src/components/BaseModal.vue
+++ vue-animation/src/components/BaseModal.vue
@@ -1,8 +1,10 @@
 <template>
-  <div class="backdrop" @click="$emit('close')"></div>
-  <dialog open>
-    <slot></slot>
-  </dialog>
+  <div>
+    <div class="backdrop" @click="$emit('close')"></div>
+    <dialog open>
+      <slot></slot>
+    </dialog>
+  </div>
 </template>
 
 <script>
@@ -34,5 +36,18 @@ dialog {
   background-color: white;
   z-index: 100;
   border: none;
+  animation: modal 0.3s ease-out forwards;
+}
+
+@keyframes modal {
+  from {
+    opacity: 0;
+    transform: translateY(-50px) scale(0.9);
+  }
+
+  to {
+    opacity: 1;
+    transform: translateY(0) scale(1);
+  }
 }
 </style>
