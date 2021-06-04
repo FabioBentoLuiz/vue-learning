0699cd6914d0f68447fb5db2260774d35aa692e8
Fabio Bento Luiz <bento_85@hotmail.com>
2021-06-04 16:13:19 +0200

    vuex getters

--- vue-vuex/src/App.vue
+++ vue-vuex/src/App.vue
@@ -1,7 +1,8 @@
 <template>
   <div>
     <base-container title="Vuex"></base-container>
-    <h3>{{ counter }}</h3>
+    <the-counter></the-counter>
+    <favorite-value></favorite-value>
     <button @click="addOne">Add 2</button>
     <button @click="increase">Increase by 10</button>
   </div>
@@ -9,10 +10,14 @@
 
 <script>
 import BaseContainer from './components/BaseContainer.vue';
+import TheCounter from './components/TheCounter.vue';
+import FavoriteValue from './components/FavoriteValue.vue';
 
 export default {
   components: {
     BaseContainer,
+    TheCounter,
+    FavoriteValue,
   },
   computed: {
     counter() {
