d080d50dea129d2c7a48262db349b6f9d3be47e0
Fabio Bento Luiz <bento_85@hotmail.com>
2021-07-21 22:07:58 +0200

    add empty components

---
+++ main-prj/src/pages/NotFound.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>
+    <h2>Page not found</h2>
+    <router-link to="/">Go to home</router-link>
+  </div>
+</template>
---
+++ main-prj/src/router.js
@@ -0,0 +1,25 @@
+import { createRouter, createWebHistory } from 'vue-router';
+import CoachesList from './pages/coaches/CoachesList.vue';
+import CoachesDetails from './pages/coaches/CoachesDetails.vue';
+import CoachesRegistration from './pages/coaches/CoachesRegistration.vue';
+import ContactCoach from './pages/requests/ContactCoach.vue';
+import RequestsReceived from './pages/requests/RequestsReceived.vue';
+import NotFound from './pages/NotFound.vue';
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [
+    { path: '/', redirect: '/coaches' },
+    { path: '/coaches', component: CoachesList },
+    {
+      path: '/coaches:id',
+      component: CoachesDetails,
+      childre: [{ path: 'contact', component: ContactCoach }]
+    },
+    { path: '/register', component: CoachesRegistration },
+    { path: '/requests', component: RequestsReceived },
+    { path: '/:notFound(.*)', component: NotFound }
+  ]
+});
+
+export default router;
